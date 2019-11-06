2ad3e7ae8b84691220c2c676399ad26786997ab2
Patrick Juaneza <pajuaneza@gmail.com>
2019-11-06 13:56:37 +0800

    Change login page

--- login.php
+++ login.php
@@ -11,17 +11,42 @@ include_once("config/appconfig.php");
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <?php include("./style.php"); ?>
+
+        <style>
+            .navbar
+            {
+                background-color: transparent;
+            }
+        </style>
     </head>
 
     <body>
+        <nav class="navbar">
+            <div class="navbar__left">
+                <div class="navbar__entry text-h4">
+                    <a href="./"><?php echo APP_NAME ?></a>
+                </div>
+            </div>
+        </nav>
+
         <main class="main-content" id="about">
             <section class="main-content__section main-content__section--small">
-                <h1>Login to <?php echo APP_NAME ?> account</h1>
-
                 <form class="form form--centered" action="./login_send.php" method="POST">
                     <div class="form__item">
-                        <label class="textbox-label" for="user">Username</label>
-                        <input class="textbox" type="text" name="user" />
+                        <h1>Create a new account</h1>
+
+                        <?php
+                        if (isset($_GET['e']))
+                        {
+                            echo <<<HTML
+                                <div>Incorrect email address/password</div>
+                            HTML;
+                        }
+                        ?>
+                    </div>
+                    <div class="form__item">
+                        <label class="textbox-label" for="user">Email address</label>
+                        <input class="textbox" type="email" name="user" />
                     </div>
 
                     <div class="form__item">
